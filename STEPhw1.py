13ce6215d3612d540c0f6c950ed0e7616ac6237f
msaitoo <m.saito017@gmail.com>
2016-05-29 20:12:14 +0100

    draft: need much editing

--- STEPhw1.py
+++ STEPhw1.py
@@ -3,16 +3,19 @@ dictionary = open('/usr/share/dict/words', 'r')
 word = dictionary.readlines()
 
 '''
-Generates the longest word from 16 characters given.
+Generates the longest word from 16 letters given.
 '''
-print ("Give 16 characters, NOT separated by space/comma/etc.")
-charac = raw_input()
+print ("Give 16 letters without space/comma/etc between them")
+let = raw_input()
 
 ans = []
-testset = set(charac)
 
 for w in word:
-    #some code here
-            ans.append(w)
+    if all(i in w for i in let):
+        #need to solve:
+        #1. exclude the ones with non-given letters
+        #2. consider duplicate letters
+        #3. don't have to use all the letters given
+        ans.append(w)
 
 #print max(ans, key=len)
