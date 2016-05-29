16f3ac2f3923b0483867e68f8808fa20b48a9528
msaitoo <m.saito017@gmail.com>
2016-05-29 19:05:18 +0100

    method2 init

--- STEPhw1.py
+++ STEPhw1.py
@@ -8,16 +8,12 @@ Generates the longest word from 16 characters given.
 print ("Give 16 characters, NOT separated by space/comma/etc.")
 charac = raw_input()
 
-poss1 = []
-for i in range(len(word)):
-    if (len(word[i])-1) <= 16:
-        poss1.append(word[i])
-poss2 = []
-poss3 = []
-for x in range(16):
-    for i in range(len(poss1)):
-        if charac[x] == poss1[i][0]:
-            poss2.append(poss1[i])
-for y in range(len(poss2)):
-        if charac[0] == poss2[y][len(poss2[y])-2]:
-            poss3.append(poss2[y])
+ans = []
+testset = set(charac)
+
+for w in word:
+    ##some code here
+        ans.append(w)
+
+ans.sort(key=lambda wd: -len(wd))
+#print ans[0]
