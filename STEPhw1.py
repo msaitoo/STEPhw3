5f238e94b21b49762ccc3eb38d0007ec49564a1f
msaitoo <m.saito017@gmail.com>
2016-05-30 19:26:09 +0100

    ver5:done. room for improvements

--- STEPhw1.py
+++ STEPhw1.py
@@ -7,26 +7,27 @@ Generates the longest word from letters given.
 print ("Give letters separated by comma")
 letter = raw_input()
 
-
 ans = []
 for w in word:
-    w = w.rstrip("\n")
+    w = w.rstrip("\n")                  #takes \n out
     sample = []
-    let = letter.split(',')
+    let = letter.split(',')             #string to list
     for i in range(len(w)):
         for x in range(len(let)):
-                if w[i] == let[x]:
-                    sample.append(1)
-                    let.remove(let[x])
-                    break
-        if len(w) == len(sample):
+            if w[0] == let[x].upper:    #takes in account for capitalised words
+                sample.append(1)
+                let.remove(let[x])
+                break
+            elif w[i] == let[x]:        #lower case words
+                sample.append(1)
+                let.remove(let[x])
+                break
+        if len(w) == len(sample):       #checks if the word is made up of let
             ans.append(w)
 
-#Problems:
-    #1. ignores capitalised words when only small letters given
-    #2. VERY SLOW :(
+#Problem: QUITE SLOW :(
 
-answer = max(ans, key=len)
+answer = max(ans, key=len)              #finds the longest word
 print ('''
 The longest word generated is [{}] with number of letters {}.
 '''.format(answer, len(answer)))
