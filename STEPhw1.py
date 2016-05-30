38e4dcdf859622c14a3234964cee9a79e45f6471
msaitoo <m.saito017@gmail.com>
2016-05-30 18:45:24 +0100

    ver1: done. room for improvement

--- STEPhw1.py
+++ STEPhw1.py
@@ -4,23 +4,27 @@ word = dictionary.readlines()
 '''
 Generates the longest word from 16 letters given.
 '''
-print ("Give 16 letters without space/comma/etc between them")
-let = raw_input()
+print ("Give 16 letters separated by comma")
+letter = raw_input()
 
 ans = []
 for w in word:
     w = w.rstrip("\n")
     sample = []
+    let = letter.split(',')
     for i in range(len(w)):
-        if len(w) <= len(let) and any(w[i] == x for x in let):  #Better way to do this?
-            sample.append(w)
-        if (len(sample) == len(w)):
+        for x in range(len(let)):
+                if w[i] == let[x]:
+                    sample.append(w)
+                    let.remove(let[x])
+                    break
+        if len(w) == len(sample):
             ans.append(w)
-            
+    
 #need to solve:
         #1. exclude the ones with non-given letters: CHECK
-        #2. consider duplicate letters
+        #2. consider duplicate letters: CHECK
         #3. capitalised words... :(
-        #4. efficiency orz
+        #4. VERY INEFICCIENT orz
 
 print max(ans, key=len)
