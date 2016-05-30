6f917d18e105cda53afe5aefba70d5be01c43093
msaitoo <m.saito017@gmail.com>
2016-05-30 18:48:32 +0100

    ver3:done. room for improvements

--- STEPhw1.py
+++ STEPhw1.py
@@ -2,9 +2,9 @@ dictionary = open('/usr/share/dict/words', 'r')
 word = dictionary.readlines()
 
 '''
-Generates the longest word from 16 letters given.
+Generates the longest word from letters given.
 '''
-print ("Give 16 letters separated by comma")
+print ("Give letters separated by comma")
 letter = raw_input()
 
 ans = []
@@ -21,10 +21,8 @@ for w in word:
         if len(w) == len(sample):
             ans.append(w)
     
-#need to solve:
-        #1. exclude the ones with non-given letters: CHECK
-        #2. consider duplicate letters: CHECK
-        #3. capitalised words... :(
-        #4. VERY INEFICCIENT orz
+#Problems:
+    #1. ignores capitalised words when only small letters given
+    #2. VERY SLOW :(
 
 print max(ans, key=len)
