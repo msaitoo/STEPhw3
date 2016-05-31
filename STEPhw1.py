1ba52911e0ece045764718ea8215333c4dc539c6
msaitoo <m.saito017@gmail.com>
2016-05-31 09:32:21 +0100

    ver5: added time and sample values

--- STEPhw1.py
+++ STEPhw1.py
@@ -15,11 +15,11 @@ for w in word:
     for i in range(len(w)):
         for x in range(len(let)):
             if w[0] == let[x].upper:    #takes in account for capitalised words
-                sample.append(1)
+                sample.append(x)
                 let.remove(let[x])
                 break
             elif w[i] == let[x]:        #lower case words
-                sample.append(1)
+                sample.append(x)
                 let.remove(let[x])
                 break
         if len(w) == len(sample):       #checks if the word is made up of let
