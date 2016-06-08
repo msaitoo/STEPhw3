91aa05fda36418b0b6730c124412f0ea5e6ab914
msaitoo <m.saito017@gmail.com>
2016-06-08 09:51:02 +0900

    Slightly neater

--- STEPhw2.py
+++ STEPhw2.py
@@ -4,38 +4,50 @@ if (len(sys.argv) != 2):
     print "usage: python %s N" % sys.argv[0]
     sys.exit()
 
-n = int(sys.argv[1])
 def initiate(n):
-    a = numpy.zeros((n,n)) # Matrix A
-    b = numpy.zeros((n,n)) # Matrix B
-    c = numpy.zeros((n,n)) # Matrix C
+    '''
+    Initiates matrices to be multiplied.
+    n = dimension of square matrices.
+    '''
+    a = numpy.zeros((n,n))     # Matrix A
+    b = numpy.zeros((n,n))     # Matrix B
+    c = numpy.zeros((n,n))     # Matrix C (result of multiplication)
     
-    #Initialise the matrices to some values.
-    for i in xrange(n):
+    for i in xrange(n):        #Initialise the matrices to some values.
         for j in xrange(n):
             a[i,j] = i * n + j
             b[i,j] = j * n + i
             c[i,j] = 0
     return (a,b,c)
-a = initiate(n)[0]
-b = initiate(n)[1]
-c = initiate(n)[2]
+
+#Initiate matrices with given dimension.
+n       = int(sys.argv[1])
+a, b, c = initiate(n)[0], initiate(n)[1], initiate(n)[2]
 
 def matrix(n):
-    begin = time.time()
+    '''
+    Multiplies two matrices.
+    n = dimension of square matrices.
+    Gives:
+            c = Resultant matrix
+            t = time taken for calculation
+    '''
+    begin = time.time()                      #Initial time
     for i in range(n):
         for j in range(n):
             elements = numpy.zeros(n)
-            for k in range(n):
+            for k in range(n):               #Sigma summation
                 elements[k] = a[i,k]*b[k,j]
-            c[i,j] = sum(elements)
-    end = time.time()
-    t = end - begin
+            c[i,j] = sum(elements)           #Elements in c
+    end = time.time()                        #End time
+    t = end - begin                          #Get time difference
     return (c,t)
-c = matrix(n)[0]
 
+#Result of multiplication
+c = matrix(n)[0]
 print "time: %.6f sec" % (matrix(n)[1])
 
+#Sum of elements in c
 total = 0
 for i in xrange(n):
     for j in xrange(n):
@@ -43,10 +55,10 @@ for i in xrange(n):
         total += c[i,j]
 print "sum: %.6f" % total
 
-#############################################################
+###########################Graphing###########################
 
 pyplot.figure()
-pyplot.title("Relationship between N and process time")
+pyplot.title("Relationship between N and time")
 pyplot.ylabel('time', fontsize = 11)
 pyplot.xlabel('N')
 x = 30
