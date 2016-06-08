c8180617eb5edd6f5aa2021985082b91d9eb320b
msaitoo <m.saito017@gmail.com>
2016-06-08 10:39:28 +0900

    cheeky code

--- STEPhw2.py
+++ STEPhw2.py
@@ -6,8 +6,8 @@ if (len(sys.argv) != 2):
 
 def initiate(n):
     '''
-    Initiates matrices to be multiplied.
-    n = dimension of square matrices.
+    Initiates square matrices to be multiplied.
+            n = dimension
     '''
     a = numpy.zeros((n,n))     # Matrix A
     b = numpy.zeros((n,n))     # Matrix B
@@ -26,8 +26,8 @@ a, b, c = initiate(n)[0], initiate(n)[1], initiate(n)[2]
 
 def matrix(n):
     '''
-    Multiplies two matrices.
-    n = dimension of square matrices.
+    Multiplies two square matrices.
+            n = dimension
     Gives:
             c = Resultant matrix
             t = time taken for calculation
@@ -40,7 +40,7 @@ def matrix(n):
                 elements[k] = a[i,k]*b[k,j]
             c[i,j] = sum(elements)           #Elements in c
     end = time.time()                        #End time
-    t = end - begin                          #Get time difference
+    t = end - begin                          #Get time difference in time
     return (c,t)
 
 #Result of multiplication
@@ -56,22 +56,27 @@ for i in xrange(n):
 print "sum: %.6f" % total
 
 ###########################Graphing###########################
+print "Would you like to graph the relation between N and time? yes/no/maybe"
+ans1 = raw_input()
+if ans1 == 'yes':
+    print "Max dimension, N, to be graphed (integer please):"
+    ans2 = int(raw_input())
+else:
+    print "Bye then... :p"
+    sys.exit()
 
 pyplot.figure()
 pyplot.title("Relationship between N and time")
-pyplot.ylabel('time', fontsize = 11)
+pyplot.ylabel('time')
 pyplot.xlabel('N')
-x = 30
+x = ans2
 X, Y = numpy.zeros(x), numpy.zeros(x)
 
 for y in range(x):
-    a = initiate(y)[0]
-    b = initiate(y)[1]
-    c = initiate(y)[2]
+    a, b, c = initiate(y)[0], initiate(y)[1], initiate(y)[2]
     t = matrix(y)[1]
-    X[y] = y
-    Y[y] = t
-pyplot.plot(X,Y)
+    X[y], Y[y] = y, t
 
+pyplot.plot(X,Y)
 pyplot.tight_layout()
 pyplot.show()
