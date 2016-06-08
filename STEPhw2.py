82c216bb78508656efdfe2c000c53369f47a15f3
msaitoo <m.saito017@gmail.com>
2016-06-08 09:31:39 +0900

    graph for dim30 auto generated

--- STEPhw2.py
+++ STEPhw2.py
@@ -5,32 +5,61 @@ if (len(sys.argv) != 2):
     sys.exit()
 
 n = int(sys.argv[1])
-a = numpy.zeros((n,n)) # Matrix A
-b = numpy.zeros((n,n)) # Matrix B
-c = numpy.zeros((n,n)) # Matrix C
+def initiate(n):
+    a = numpy.zeros((n,n)) # Matrix A
+    b = numpy.zeros((n,n)) # Matrix B
+    c = numpy.zeros((n,n)) # Matrix C
+    
+    #Initialise the matrices to some values.
+    for i in xrange(n):
+        for j in xrange(n):
+            a[i,j] = i * n + j
+            b[i,j] = j * n + i
+            c[i,j] = 0
+    return (a,b,c)
+a = initiate(n)[0]
+b = initiate(n)[1]
+c = initiate(n)[2]
 
-#Initialise the matrices to some values.
-for i in xrange(n):
-    for j in xrange(n):
-        a[i,j] = i * n + j
-        b[i,j] = j * n + j
-        c[i,j] = 0
+def matrix(n):
+    begin = time.time()
+    for i in range(n):
+        for j in range(n):
+            elements = numpy.zeros(n)
+            for k in range(n):
+                elements[k] = a[i,k]*b[k,j]
+            c[i,j] = sum(elements)
+    end = time.time()
+    t = end - begin
+    return (c,t)
+c = matrix(n)[0]
 
-begin = time.time()
-
-for i in range(n):
-    for j in range(n):
-        elements = numpy.zeros(n)
-        for k in range(n):
-            elements[k] = a[i,k]*b[k,j]
-        c[i,j] = sum(elements)
-
-end = time.time()
-print "time: %.6f sec" % (end - begin)
+print "time: %.6f sec" % (matrix(n)[1])
 
 total = 0
 for i in xrange(n):
     for j in xrange(n):
-        print c[i,j]
+        #print c[i,j]
         total += c[i,j]
 print "sum: %.6f" % total
+
+#############################################################
+
+pyplot.figure()
+pyplot.title("Relationship between N and process time")
+pyplot.ylabel('time', fontsize = 11)
+pyplot.xlabel('N')
+x = 30
+X, Y = numpy.zeros(x), numpy.zeros(x)
+
+for y in range(x):
+    a = initiate(y)[0]
+    b = initiate(y)[1]
+    c = initiate(y)[2]
+    t = matrix(y)[1]
+    X[y] = y
+    Y[y] = t
+pyplot.plot(X,Y)
+
+pyplot.tight_layout()
+pyplot.show()
